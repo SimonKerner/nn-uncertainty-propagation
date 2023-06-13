d3a234f96c20219ee67bc2977c7e484417b8c594
SimonKerner <104328359+SimonKerner@users.noreply.github.com>
2023-06-13 12:40:33 +0200

    update files

--- uncertainty_propagation.py
+++ uncertainty_propagation.py
@@ -7,6 +7,7 @@ Created on Fri Jun  2 17:24:32 2023
 
 import os
 import pickle
+import random
 
 import utils
 
@@ -23,7 +24,9 @@ from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 #from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.neighbors import KernelDensity
 
 from scipy.stats import gaussian_kde
 
@@ -39,8 +42,8 @@ from scipy.stats import gaussian_kde
 datasets = os.path.join(os.getcwd(), 'datasets')
 
     
-with open(os.path.join(datasets, "data_australian.pkl"), 'rb') as file:
-    data_australian = pickle.load(file)
+#with open(os.path.join(datasets, "data_australian.pkl"), 'rb') as file:
+#    data_australian = pickle.load(file)
     
     
 with open(os.path.join(datasets, "saved_pdfs_australian.pkl"), 'rb') as file:
@@ -63,7 +66,7 @@ original_col_names = ["0", "1", "2", "3", "4", "5" , "6", "7", "8", "9", "10", "
 
 # rename columns
 data.columns = column_names
-data_australian.columns = column_names
+#data_australian.columns = column_names
     
 
 
@@ -79,6 +82,7 @@ DATAFRAME = data
 # set random state
 RANDOM_STATE = 24
 np.random.seed(RANDOM_STATE)
+np.random.RandomState(RANDOM_STATE)
 tf.random.set_seed(RANDOM_STATE)
 
 
@@ -119,6 +123,116 @@ if visiualize:
     plt.show()
     distplot_plot.savefig(os.path.join(images, 'distplot_plot.png'))
     plt.close()
+    
+    
+    
+    # Plotting combined distribution using box plots
+    DATAFRAME.boxplot(column=column_names, figsize=(12, 6))
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+    
+    
+    
+    # Plotting combined distribution using histograms
+    DATAFRAME.hist(column=column_names, bins=10, figsize=(12, 10))
+    plt.tight_layout()
+    plt.show()
+    
+    
+    # Visualizing correlation between variables using a heatmap
+    corr_matrix = DATAFRAME.corr()
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", square=True)
+    plt.tight_layout()
+    plt.show()
+    
+    
+    
+    # Create a KDE plot for each column
+    for column in column_names:
+        plt.figure(figsize=(8, 4))
+        sns.kdeplot(data=DATAFRAME[column], fill=True, color='skyblue', alpha=0.5)
+        plt.xlabel(column)
+        plt.ylabel('Density')
+        plt.title(f'KDE Plot of {column}')
+        plt.tight_layout()
+        plt.show()
+        
+        
+
+    # Create a combined KDE plot
+    plt.figure(figsize=(12, 6))
+    for column in column_names:
+        sns.kdeplot(data=DATAFRAME[column], fill=True, label=column)
+    plt.xlabel('Values')
+    plt.ylabel('Density')
+    plt.title('Combined KDE Plot')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    
+    
+    combined_kde = None
+    for column in column_names:
+        # Calculate the KDE for the current column
+        data = DATAFRAME[column]
+        kde = gaussian_kde(data)
+        x = np.linspace(data.min(), data.max(), 1000)
+        y = kde(x)
+        
+        if combined_kde is None:
+            combined_kde = np.zeros_like(y)
+        
+        # Add the KDE estimates to the combined KDE
+        combined_kde += y
+    
+    # Plot the combined KDE
+    plt.plot(x, combined_kde, label="Combined KDE")
+    plt.xlabel('Values')
+    plt.ylabel('Density')
+    plt.title('Combined KDE Plot')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    
+    
+    
+    
+    # Calculate the entropy for each feature
+    entropies = []
+    target_column = "0 - Reject / 1 - Accept"
+    target_counts = DATAFRAME[target_column].value_counts()
+    total_samples = target_counts.sum()
+
+    for column in column_names:
+        feature_entropy = 0
+        feature_counts = DATAFRAME.groupby([column, target_column]).size()
+        unique_values = DATAFRAME[column].unique()
+
+        for value in unique_values:
+            value_counts = feature_counts[value]
+            value_total = value_counts.sum()
+            value_probabilities = value_counts / value_total
+            value_entropy = -np.sum(value_probabilities * np.log2(value_probabilities))
+            feature_entropy += (value_total / total_samples) * value_entropy
+
+        entropies.append(feature_entropy)
+
+    # Find the index of the feature with the highest entropy
+    most_uncertain_feature_index = np.argmax(entropies)
+    most_uncertain_feature = column_names[most_uncertain_feature_index]
+
+    print("Most Uncertain Feature:", most_uncertain_feature)
+
+    # Plot the entropies
+    plt.figure(figsize=(6, 10))
+    plt.barh(column_names, entropies)
+    plt.xlabel("Entropy")
+    plt.ylabel("Features")
+    plt.title("Entropy of Features")
+    plt.show()
     
 
 
@@ -163,16 +277,20 @@ if training:
     # build model
     model = keras.Model(inputs=inputs, outputs=outputs)
     
+    
     # compile model
     model.compile(optimizer=keras.optimizers.Adam(),
                   loss=keras.losses.BinaryCrossentropy(),
                   metrics=["accuracy"])
     
+    
     # fit model
     model_history = model.fit(X_train, y_train, validation_data=[X_test, y_test], batch_size=15, epochs=50)
     
+    
     # plot model
     utils.plot_history(model_history)
+
 
     # save new model
     model.save(os.path.join(models, "standard_model"))
@@ -196,34 +314,67 @@ model = keras.models.load_model(os.path.join(models, "standard_model"))
 # some metric for the predictions
 ##########################################################################################################################
 
- 
+
 standard_predictions_sig = model.predict(X)
 standard_predictions_scalar = (standard_predictions_sig>0.5).astype("int32")
+
 
 metrics = True
 
 if metrics:
     
-    # output
-    sns.kdeplot(standard_predictions_sig)
-    
-    # scores
-    #accuracy = accuracy_score(y, standard_predictions_scalar)
-    #precision = precision_score(y, standard_predictions_scalar)
-    report = classification_report(y, standard_predictions_scalar, target_names=['Rejected', 'Accepted'])
-    
-    # Confusion Matrix
-    confusion_matrix = confusion_matrix(y, standard_predictions_scalar)
-    display = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=['Rejected', 'Accepted'])
-    display.plot()
-    plt.savefig(os.path.join(images, 'ConfusionMatrix_Standard.png'))
+    utils.create_metrics(y, standard_predictions_scalar)
 
 
 
 
 ##########################################################################################################################
-# 
+# add missing data
 ##########################################################################################################################
+
+
+
+
+
+# create new Dataset with random missing values
+DATAFRAME_MISS = utils.add_missing_values(DATAFRAME.iloc[:, :14], 0.1, RANDOM_STATE) 
+DATAFRAME_MISS = DATAFRAME_MISS.merge(DATAFRAME.iloc[:,-1], left_index=True, right_index=True)
+
+
+
+
+##########################################################################################################################
+# sample Kernel Density Estimate over missing dataset
+##########################################################################################################################
+
+
+"""
+kernel_types = {
+    "Sex": "exponential",
+    "Age": "exponential",
+    "Mean time at addresses": "exponential",
+    "Home status": "gaussian",
+    "Current occupation": "gaussian",
+    "Current job status": "gaussian",
+    "Mean time with employers": "exponential",
+    "Other investments": "gaussian",
+    "Bank account": "gaussian",
+    "Time with bank": "gaussian",
+    "Liability reference": "exponential",
+    "Account reference": "exponential",
+    "Monthly housing expense": "gaussian",
+    "Savings account balance": "gaussian"
+}
+"""
+
+
+
+ 
+Imputed_df = utils.kde_Imputer(DATAFRAME_MISS, bandwidth=0.1)
+
+#neg = (Imputed_df<0).sum()
+
+
 
 
 
--- utils.py
+++ utils.py
@@ -7,51 +7,281 @@ Created on Sun Jun 11 10:25:12 2023
 
 
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+import pandas as pd
 
+import numpy as np
+import random
+
+from sklearn.neighbors import KernelDensity
+
+import seaborn as sns
 
 
 
 ##########################################################################################################################
 # Define helper functions
 ##########################################################################################################################
+    
+    
+def plot_history(history, smooth=False, log_scale=False):
+    
+    """
+    Plots the training and validation metrics from the training history.
+    
+    Args:
+        history (History): Training history object.
+        smooth (bool): Whether to apply smoothing to the metrics curves. Defaults to False.
+        log_scale (bool): Whether to use a logarithmic scale on the y-axis. Defaults to False.
+    
+    """
+    
+    metric_labels = {
+        'loss': 'Loss',
+        'accuracy': 'Accuracy',
+        'mse': 'Mean Squared Error',
+        'mae': 'Mean Absolute Error',
+        'mape': 'Mean Absolute Percentage Error',
+        'msle': 'Mean Squared Logarithmic Error',
+        'cosine_similarity': 'Cosine Similarity'
+    }
 
+    num_metrics = len(history.history)
+    subplot_rows = (num_metrics + 1) // 2  # Adjust the subplot layout based on the number of metrics
 
+    fig, axes = plt.subplots(subplot_rows, 2, figsize=(12, 4 * subplot_rows))
 
-def plot_history(history):
-    loss_list = [s for s in history.history.keys() if 'loss' in s and 'val' not in s]
-    val_loss_list = [s for s in history.history.keys() if 'loss' in s and 'val' in s]
-    acc_list = [s for s in history.history.keys() if 'acc' in s and 'val' not in s]
-    val_acc_list = [s for s in history.history.keys() if 'acc' in s and 'val' in s]
-    
-    if len(loss_list) == 0:
-        print('Loss is missing in history')
-        return 
-    
-    ## As loss always exists
-    epochs = range(1,len(history.history[loss_list[0]]) + 1)
-    
-    ## Loss
-    plt.figure(1)
-    for l in loss_list:
-        plt.plot(epochs, history.history[l], 'b', label='Training loss (' + str(str(format(history.history[l][-1],'.5f'))+')'))
-    for l in val_loss_list:
-        plt.plot(epochs, history.history[l], 'g', label='Validation loss (' + str(str(format(history.history[l][-1],'.5f'))+')'))
-    
-    plt.title('Loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    
-    ## Accuracy
-    plt.figure(2)
-    for l in acc_list:
-        plt.plot(epochs, history.history[l], 'b', label='Training accuracy (' + str(format(history.history[l][-1],'.5f'))+')')
-    for l in val_acc_list:    
-        plt.plot(epochs, history.history[l], 'g', label='Validation accuracy (' + str(format(history.history[l][-1],'.5f'))+')')
-    
-    plt.title('Accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
+    idx = 0  # Keep track of the current subplot index
+
+    for metric, values in history.history.items():
+        if metric in metric_labels:
+            row = idx // 2
+            col = idx % 2
+
+            ax = axes[row, col]
+
+            if smooth:
+                metric_values = smooth_curve(values)
+            else:
+                metric_values = values
+
+            ax.plot(range(1, len(metric_values) + 1), metric_values, label='Training ' + metric_labels[metric])
+
+            val_metric = 'val_' + metric
+            if val_metric in history.history:
+                val_metric_values = history.history[val_metric]
+                ax.plot(range(1, len(val_metric_values) + 1), val_metric_values, label='Validation ' + metric_labels[metric])
+
+            ax.set_title(metric_labels[metric])
+
+            if log_scale:
+                ax.set_yscale('log')
+
+            ax.set_xlabel('Epochs')
+            ax.set_ylabel(metric_labels[metric])
+            ax.legend()
+
+            idx += 1  # Increment the subplot index
+
+    # Remove empty subplots
+    while idx < subplot_rows * 2:
+        row = idx // 2
+        col = idx % 2
+        fig.delaxes(axes[row, col])
+        idx += 1
+
+    plt.tight_layout()
     plt.show()
-    plt.close()
+
+
+def smooth_curve(points, factor=0.8):
+    
+    """
+    Applies smoothing to a list of points using exponential moving average.
+    
+    Args:
+        points (list): List of numeric values.
+        factor (float): Smoothing factor. Defaults to 0.8.
+    
+    Returns:
+        list: Smoothed list of points.
+    
+    """
+    
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+
+
+
+def create_metrics(y_true, predictions):
+    
+    """
+    Calculates various evaluation metrics based on the true labels and predicted values.
+    
+    Args:
+        y_true (array-like): True labels.
+        predictions (array-like): Predicted values.
+    
+    Returns:
+        dict: Dictionary containing the calculated metrics.
+
+    """
+    
+    # Scores
+    accuracy = accuracy_score(y_true, predictions)
+    precision = precision_score(y_true, predictions)
+    recall = recall_score(y_true, predictions)
+    f1 = f1_score(y_true, predictions)
+    report = classification_report(y_true, predictions, target_names=['Rejected', 'Accepted'])
+
+    # Confusion Matrix
+    cm = confusion_matrix(y_true, predictions)
+    display_labels = ['Rejected', 'Accepted']
+    display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=display_labels)
+    display.plot()
+
+    # Additional Metrics
+    metrics = {
+        'accuracy': accuracy,
+        'precision': precision,
+        'recall': recall,
+        'f1-score': f1,
+        'classification_report': report,
+        'confusion_matrix': cm,
+    }
+
+    # Print Additional Metrics
+    for metric, value in metrics.items():
+        print(f'{metric.capitalize()}:')
+        print(value)
+        print()
+
+    return metrics
+
+
+
+
+def add_missing_values(df, miss_rate, random_seed=None):
+    
+    """
+    Adds missing values to a DataFrame based on the specified missing rate.
+    
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+        miss_rate (float): Proportion of missing values to add, between 0 and 1.
+        random_seed (int): Random seed for reproducibility. Defaults to None.
+    
+    Returns:
+        pd.DataFrame: DataFrame with missing values added.
+    
+    """
+    
+    df = df.copy()
+    
+    ix = [(row, col) for row in range(df.shape[0]) for col in range(df.shape[1])]
+    
+    random.seed(random_seed)
+    
+    for row, col in random.sample(ix, int(round(miss_rate * len(ix)))):
+        df.iat[row, col] = np.nan
+        
+    return df
+
+
+
+
+
+def kde_Imputer(dataframe, kernel="gaussian", bandwidth="scott", random_state=None, print_info=False):
+    
+    """
+    Imputes missing values in a DataFrame using Kernel Density Estimation (KDE).
+    
+    Args:
+        dataframe (pd.DataFrame): Input DataFrame.
+        kernel (str or dict): Kernel type or a mapping of column names to kernel types.
+                              Defaults to "gaussian".
+        bandwidth (str, float): Bandwidth estimation method or a fixed bandwidth value. Defaults to "scott".
+        random_state (int): Random seed for reproducibility. Defaults to None.
+        print_info (bool): Whether to print information about the KDE parameters. Defaults to True.
+    
+    Returns:
+        pd.DataFrame: DataFrame with missing values imputed using KDE.
+    
+    Raises:
+        ValueError: If invalid arguments are provided.
+    
+    """
+    
+    # Input validation
+    if not isinstance(dataframe, pd.DataFrame):
+        raise ValueError("The 'dataframe' argument must be a valid pandas DataFrame.")
+    
+    valid_kernels = ["gaussian", "tophat", "epanechnikov", "exponential", "linear", "cosine"]
+    if isinstance(kernel, str) and kernel not in valid_kernels:
+        raise ValueError(f"Invalid kernel type '{kernel}'. Valid options are: {', '.join(valid_kernels)}.")
+    
+    if isinstance(kernel, dict):
+        for col, k in kernel.items():
+            if k not in valid_kernels:
+                raise ValueError(f"Invalid kernel type '{k}' for column '{col}'. Valid options are: {', '.join(valid_kernels)}.")
+    
+    if not isinstance(bandwidth, (str, float)):
+        raise ValueError("The 'bandwidth' argument must be a string or a float.")
+    
+    if isinstance(kernel, str):
+        kernel_mapping = {column: kernel for column in dataframe.columns}
+    elif isinstance(kernel, dict):
+        kernel_mapping = kernel
+
+
+    imputed_df = dataframe.copy()
+    
+    for column in dataframe.columns:
+        values = dataframe[column].dropna().values.reshape(-1, 1)
+        
+        kde = KernelDensity(kernel=kernel_mapping.get(column), bandwidth=bandwidth)
+        kde.fit(values)
+        
+        if print_info:
+            # Print the KernelDensity parameters for the current column
+            print(f"Column: {column}")
+            print(f"Kernel: {kde.kernel}")
+            print(f"Bandwidth: {kde.bandwidth}\n")
+
+            # KDE Plot of column without missing data
+            plt.figure(figsize=(8, 4))
+            sns.kdeplot(data=values, fill=True, color='skyblue', alpha=0.5)
+            plt.xlabel(column)
+            plt.ylabel('Density')
+            plt.title(f'KDE Plot of {column}')
+            plt.tight_layout()
+            plt.show()
+
+        missing_values = imputed_df[column].isnull()
+        num_missing = missing_values.sum()
+
+        if num_missing > 0:
+            kde_samples = kde.sample(num_missing, random_state=random_state)    
+            
+            # Limit samples to the range of 0 and 1 for binary columns
+            if np.array_equal(np.unique(values), [0., 1.]):
+                
+                kde_samples = np.random.choice(np.clip(kde_samples, a_min=0., a_max=1.).flatten(), num_missing, replace=True)
+            
+            # if original columns do not have negative values, clip at lower limit
+            elif (values<0).sum() == 0:
+    
+                kde_samples = np.random.choice(np.clip(kde_samples, a_min=0., a_max=None).flatten(), num_missing, replace=True)
+                      
+            kde_samples = np.random.choice(kde_samples.reshape(-1), num_missing, replace=True)  # Reshape to match missing values
+            imputed_df.loc[missing_values, column] = kde_samples
+
+    return imputed_df
