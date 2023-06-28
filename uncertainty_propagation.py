9c0bbcea0f4e39617dcc8fd4fb3677ef6cc21f28
SimonKerner <104328359+SimonKerner@users.noreply.github.com>
2023-06-28 12:23:12 +0200

    updated files

--- uncertainty_propagation.py
+++ uncertainty_propagation.py
@@ -125,15 +125,12 @@ if visiualize:
     plt.close()
     
     
-    
     # Plotting combined distribution using box plots
     DATAFRAME.boxplot(column=column_names, figsize=(12, 6))
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
 
-    
-    
     
     # Plotting combined distribution using histograms
     DATAFRAME.hist(column=column_names, bins=10, figsize=(12, 10))
@@ -149,7 +146,6 @@ if visiualize:
     plt.show()
     
     
-    
     # Create a KDE plot for each column
     for column in column_names:
         plt.figure(figsize=(8, 4))
@@ -161,7 +157,6 @@ if visiualize:
         plt.show()
         
         
-
     # Create a combined KDE plot
     plt.figure(figsize=(12, 6))
     for column in column_names:
@@ -188,6 +183,7 @@ if visiualize:
         # Add the KDE estimates to the combined KDE
         combined_kde += y
     
+    
     # Plot the combined KDE
     plt.plot(x, combined_kde, label="Combined KDE")
     plt.xlabel('Values')
@@ -198,8 +194,7 @@ if visiualize:
     plt.show()
     
     
-    
-    
+    """
     # Calculate the entropy for each feature
     entropies = []
     target_column = "0 - Reject / 1 - Accept"
@@ -233,7 +228,8 @@ if visiualize:
     plt.ylabel("Features")
     plt.title("Entropy of Features")
     plt.show()
-    
+    """
+
 
 
 
@@ -319,7 +315,7 @@ standard_predictions_sig = model.predict(X)
 standard_predictions_scalar = (standard_predictions_sig>0.5).astype("int32")
 
 
-metrics = True
+metrics = False
 
 if metrics:
     
@@ -331,9 +327,6 @@ if metrics:
 ##########################################################################################################################
 # add missing data
 ##########################################################################################################################
-
-
-
 
 
 # create new Dataset with random missing values
@@ -368,8 +361,6 @@ kernel_types = {
 """
 
 
-
- 
 Imputed_df = utils.kde_Imputer(DATAFRAME_MISS, bandwidth=0.1)
 
 #neg = (Imputed_df<0).sum()
@@ -378,10 +369,231 @@ Imputed_df = utils.kde_Imputer(DATAFRAME_MISS, bandwidth=0.1)
 
 
 
+"""
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# Load the complete dataset and the KDE imputed dataset
+complete_data = DATAFRAME
+imputed_data = Imputed_df
+
+# Split the data into features (X) and target variable (y)
+X_complete = complete_data.drop("0 - Reject / 1 - Accept", axis=1)
+y_complete = complete_data["0 - Reject / 1 - Accept"]
+
+X_imputed = imputed_data.drop("0 - Reject / 1 - Accept", axis=1)
+y_imputed = imputed_data["0 - Reject / 1 - Accept"]
+
+# Convert the data to numpy arrays
+X_complete = X_complete.values
+y_complete = y_complete.values
+
+X_imputed = X_imputed.values
+y_imputed = y_imputed.values
+
+# Define the architecture of the sequential feed-forward network
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(X_complete.shape[1],)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# Compile the model with an appropriate loss function and optimizer
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model on the complete dataset
+model.fit(X_complete, y_complete, epochs=10, batch_size=32, verbose=1)
+
+# Evaluate the model on the imputed dataset
+loss, accuracy = model.evaluate(X_imputed, y_imputed, verbose=1)
+print("Accuracy on the imputed dataset:", accuracy)
+
+# Get the predicted probabilities for the complete dataset
+predicted_probabilities_complete = model.predict(X_complete)
+
+# Get the predicted probabilities for the imputed dataset
+predicted_probabilities = model.predict(X_imputed)
+
+# Calculate the uncertainty measures (e.g., variance and entropy)
+uncertainty_variance = np.var(predicted_probabilities)
+uncertainty_entropy = -np.sum(predicted_probabilities * np.log2(predicted_probabilities))
+
+print("Uncertainty variance:", uncertainty_variance)
+print("Uncertainty entropy:", uncertainty_entropy)
+
+
+# Create a histogram of predicted probabilities for the complete dataset
+plt.figure(figsize=(8, 6))
+plt.hist(predicted_probabilities_complete, bins=20, alpha=0.5, label='Complete Dataset')
+
+# Create a histogram of predicted probabilities for the imputed dataset
+plt.hist(predicted_probabilities, bins=20, alpha=0.5, label='KDE Imputed Dataset')
+
+plt.xlabel('Predicted Probability')
+plt.ylabel('Frequency')
+plt.title('Output Distribution Comparison')
+plt.legend()
+plt.show()
+
+
+
+
+# Create a KDE plot for the predicted probabilities of the complete dataset
+sns.kdeplot(predicted_probabilities_complete.flatten(), label='Complete Dataset')
+
+# Create a KDE plot for the predicted probabilities of the imputed dataset
+sns.kdeplot(predicted_probabilities.flatten(), label='KDE Imputed Dataset')
+
+plt.xlabel('Predicted Probability')
+plt.ylabel('Density')
+plt.title('Output Distribution Comparison')
+plt.legend()
+plt.show()
+"""
+
+
+from scipy.stats import norm, beta  # For fitting parametric distributions
+from sklearn.neighbors import KernelDensity  # For non-parametric kernel density estimation
+import matplotlib.pyplot as plt  # For visualization
+
+
+def generate_input_samples(data, column_names, continuous_features, nominal_features, plot_distributions=True):
+    """
+    Generate input samples according to the desired probability distributions.
+
+    Args:
+        data (pandas.DataFrame): The dataset containing the values for each attribute.
+        column_names (list): List of column names in the dataset.
+        continuous_features (list): List of column names for continuous features.
+        nominal_features (list): List of column names for nominal features.
+
+    Returns:
+        numpy.ndarray: Generated input samples.
+    """
+    features = data.iloc[:, :-1].values  # Select all columns except the last as features
+
+    # Calculate distribution parameters for continuous features
+    distribution_params = {}
+    for feature in continuous_features:
+        mean = data[feature].mean()
+        std = data[feature].std()
+        distribution_params[feature] = {"mean": mean, "std": std}
+
+    # Generate input samples for each feature
+    input_samples = []
+    for feature in column_names:
+        if feature in continuous_features:
+            # Continuous feature
+            mean = distribution_params[feature]['mean']
+            std = distribution_params[feature]['std']
+            samples = np.random.normal(mean, std, size=len(features))
+        else:
+            # Nominal feature
+            categories = data[feature].unique()
+            samples = np.random.choice(categories, size=len(features), replace=True)
+        input_samples.append(samples)
+        
+        # Plot the distribution if required
+        if plot_distributions:
+            plt.figure()
+            plt.hist(samples, bins=30)
+            plt.title(f"Distribution of {feature}")
+            plt.xlabel("Value")
+            plt.ylabel("Frequency")
+            plt.show()
+
+    # Combine all the generated samples
+    input_samples = np.column_stack(input_samples)
+    return input_samples
+
+
+
+# Load your dataset into a pandas DataFrame
+data = Imputed_df
+
+# Define the necessary parameters
+column_names = ["Sex", "Age", "Mean time at adresses", "Home status", "Current occupation",
+                "Current job status", "Mean time with employers", "Other investments",
+                "Bank account", "Time with bank", "Liability reference", "Account reference",
+                "Monthly housing expense", "Savings account balance"]
+
+continuous_features = ["Age", "Mean time at adresses", "Mean time with employers",
+                       "Time with bank", "Monthly housing expense", "Savings account balance"]
+
+nominal_features = ["Sex", "Home status", "Current occupation", "Current job status",
+                    "Other investments", "Bank account", "Liability reference", "Account reference"]
+
+# Generate input samples
+input_samples = generate_input_samples(data, column_names, continuous_features, nominal_features)
+
+
+# Calculate the output probabilities and output labels
+output_probs = model.predict(input_samples)
+output_probs = output_probs.flatten()  # Flatten to 1D array
+output_labels = np.round(output_probs).astype(int)
 
 
 
 
 
+# Visualize the output distributions for each label
+for label in np.unique(output_labels):
+    label_probs = output_probs[output_labels == label]
+
+    # Fit Gaussian distribution to label probabilities
+    mean, std = norm.fit(label_probs)
+
+    # Visualize the distribution
+    plt.figure()
+    plt.hist(label_probs, bins=30, density=True, alpha=0.6, label=f'Label {label}')
+    x = np.linspace(0, 1, 100)
+    plt.plot(x, norm.pdf(x, mean, std), 'r-', label='Gaussian Fit')
+    plt.xlabel('Output Probabilities')
+    plt.ylabel('Density')
+    plt.legend()
+    plt.show()
+
+    # Perform kernel density estimation
+    kde = KernelDensity(bandwidth=0.05).fit(label_probs.reshape(-1, 1))
+    x = np.linspace(0, 1, 100)
+    log_density = kde.score_samples(x.reshape(-1, 1))
+    density = np.exp(log_density)
+
+    # Visualize the distribution
+    plt.figure()
+    plt.plot(x, density, label=f'Label {label}')
+    plt.xlabel('Output Probabilities')
+    plt.ylabel('Density')
+    plt.legend()
+    plt.show()
+    
+    
+# Fit Gaussian distribution to output probabilities
+mean, std = norm.fit(output_probs)
+
+# Visualize the distribution
+plt.figure()
+plt.hist(output_probs, bins=30, density=True, alpha=0.6, color='g')
+x = np.linspace(0, 1, 100)
+plt.plot(x, norm.pdf(x, mean, std), 'r-', label='Gaussian Fit')
+plt.xlabel('Output Probabilities')
+plt.ylabel('Density')
+plt.legend()
+plt.show()
+
+# Perform kernel density estimation
+kde = KernelDensity(bandwidth=0.05).fit(output_probs.reshape(-1, 1))
+x = np.linspace(0, 1, 100)
+log_density = kde.score_samples(x.reshape(-1, 1))
+density = np.exp(log_density)
+
+# Visualize the distribution
+plt.figure()
+plt.plot(x, density, 'b-', label='Kernel Density Estimation')
+plt.xlabel('Output Probabilities')
+plt.ylabel('Density')
+plt.legend()
+plt.show()
 
 
