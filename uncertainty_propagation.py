80b783d11b88f52e19711cd980b6ce7a2547160c
SimonKerner <104328359+SimonKerner@users.noreply.github.com>
2023-07-04 17:17:47 +0200

    update files

--- uncertainty_propagation.py
+++ uncertainty_propagation.py
@@ -6,6 +6,7 @@ Created on Fri Jun  2 17:24:32 2023
 """
 
 import os
+import sys
 import pickle
 import random
 
@@ -34,94 +35,114 @@ from scipy.stats import gaussian_kde
 
 
 ##########################################################################################################################
-# load datasets
+# constant settings and paths
 ##########################################################################################################################
 
-
-# set path to dataframes
-datasets = os.path.join(os.getcwd(), 'datasets')
-
-    
-#with open(os.path.join(datasets, "data_australian.pkl"), 'rb') as file:
-#    data_australian = pickle.load(file)
-    
-    
-with open(os.path.join(datasets, "saved_pdfs_australian.pkl"), 'rb') as file:
-    data_australian_pdf = pickle.load(file)
-    
-    
-with open(os.path.join(datasets, "australian.dat"), 'rb') as file:
-    data = pd.read_table(file, sep=" ", engine="python", header=None)
-    
-    
-# rename columns   
-column_names=["Sex", "Age", "Mean time at adresses", "Home status", "Current occupation",
-          "Current job status", "Mean time with employers", "Other investments",
-          "Bank account", "Time with bank", "Liability reference", "Account reference",
-          "Monthly housing expense", "Savings account balance", "0 - Reject / 1 - Accept"]
+#choose working dataset: "australian" or "climate_simulation"
+dataset = "climate_simulation" 
 
 
-original_col_names = ["0", "1", "2", "3", "4", "5" , "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+# set path to different folders
+dataset_path = os.path.join(os.getcwd(), 'datasets')
+image_path = os.path.join(os.getcwd(), 'images')
+models = os.path.join(os.getcwd(), 'models')
 
-
-# rename columns
-data.columns = column_names
-#data_australian.columns = column_names
-    
-
-
-
-##########################################################################################################################
-# set Variables to work with + standardization of values for better performance
-##########################################################################################################################
-    
-
-# choose dataframe to work with
-DATAFRAME = data 
 
 # set random state
-RANDOM_STATE = 24
+RANDOM_STATE = None
 np.random.seed(RANDOM_STATE)
 np.random.RandomState(RANDOM_STATE)
 tf.random.set_seed(RANDOM_STATE)
 
 
-# use data scaler to norm the data
-scaler = MinMaxScaler()
-standardized = scaler.fit_transform(DATAFRAME)
+# constans for further processing
+standardize_data = True
+visiualize_data = False
 
-# change to dataframe
-DATAFRAME = pd.DataFrame(standardized)
-DATAFRAME.columns = column_names
+# train or load model
+train_model = False
+load_model = True
+
+
+get_prediction_metrics = True
 
 
 
 
 ##########################################################################################################################
-# visiualize Dataframe 
+# load datasets
+##########################################################################################################################
+    
+# load data for australian credit card approval dataset
+if dataset == "australian":
+    
+    with open(os.path.join(dataset_path, dataset + ".dat"), 'rb') as DATAFRAME:
+        DATAFRAME = pd.read_table(DATAFRAME, sep=" ", engine="python", header=None)
+    
+    # rename columns   
+    column_names=["Sex", "Age", "Mean time at adresses", "Home status", "Current occupation",
+              "Current job status", "Mean time with employers", "Other investments",
+              "Bank account", "Time with bank", "Liability reference", "Account reference",
+              "Monthly housing expense", "Savings account balance", "0 - Reject / 1 - Accept"]
+    
+    
+    original_col_names = ["0", "1", "2", "3", "4", "5" , "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+    
+    # rename columns
+    DATAFRAME.columns = column_names
+
+
+# load data for climate modal simulation crashes dataset
+if dataset == "climate_simulation":
+    
+    with open(os.path.join(dataset_path, dataset + ".dat"), 'rb') as DATAFRAME:
+        DATAFRAME = pd.read_table(DATAFRAME, sep="\s+", engine='python', header = 0)
+    
+    # drop the first two elements of the dataset -> not relevant
+    DATAFRAME = DATAFRAME.iloc[:, 2:]
+
+    column_names = DATAFRAME.columns.to_list()
+
+
+
+
+##########################################################################################################################
+# standardization of values for better performance
+##########################################################################################################################
+    
+
+if standardize_data:
+    # use data scaler to norm the data
+    scaler = MinMaxScaler()
+    standardized = scaler.fit_transform(DATAFRAME)
+    
+    # change to dataframe
+    DATAFRAME = pd.DataFrame(standardized)
+    DATAFRAME.columns = column_names
+
+
+
+
+##########################################################################################################################
+# visiualize_data Dataframe 
 ##########################################################################################################################
 
 
-images = os.path.join(os.getcwd(), 'images')
-
-
-visiualize = False
-
-if visiualize:
+if visiualize_data:
     # plot Scatter matrix of dataframe
     sns.set_style("whitegrid")
     sns_plot = sns.PairGrid(DATAFRAME, diag_sharey=False, corner=False)
     sns_plot.map_lower(sns.kdeplot, levels=4, color = "red", fill=True, alpha=0.4)
     sns_plot.map_lower(sns.scatterplot, s=15)
     sns_plot.map_diag(sns.kdeplot, fill=True)
-    plt.savefig(os.path.join(images, 'dataset_plot.png'))
+    plt.savefig(os.path.join(image_path, 'dataset_plot.png'))
 
 
     # Density and Histplot for exploration
     distplot_data = DATAFRAME.melt(var_name='column_names')
     distplot_plot = sns.displot(data=distplot_data, x='value', col='column_names', col_wrap=5, rug=False, kde=True, stat='density')
     plt.show()
-    distplot_plot.savefig(os.path.join(images, 'distplot_plot.png'))
+    distplot_plot.savefig(os.path.join(image_path, 'distplot_plot.png'))
     plt.close()
     
     
@@ -238,13 +259,8 @@ if visiualize:
 ##########################################################################################################################
 
 
-# not standardized dataframe
-#X = data.iloc[:, 0:14]
-#y = data["0 - Reject / 1 - Accept"]
-
-# standardized dataframe
-X = DATAFRAME.iloc[:, 0:14]
-y = DATAFRAME["0 - Reject / 1 - Accept"]
+X = DATAFRAME.iloc[:, 0:-1]
+y = DATAFRAME[column_names[-1]]
 
 
 X_train, X_test, y_train,  y_test = train_test_split(X, y, test_size=0.25, random_state=RANDOM_STATE)
@@ -257,15 +273,9 @@ X_train, X_test, y_train,  y_test = train_test_split(X, y, test_size=0.25, rando
 ##########################################################################################################################
 
 
-# set path to models
-models = os.path.join(os.getcwd(), 'models')
-
-
-training = False
-
-if training:
+if train_model:
     # layers of the network
-    inputs = keras.Input(shape=(14,))
+    inputs = keras.Input(shape=(X.shape[1],))
     x = layers.Dense(32, activation='relu')(inputs)
     x = layers.Dense(16, activation='relu')(x)
     outputs = layers.Dense(1, activation='sigmoid')(x)
@@ -289,7 +299,7 @@ if training:
 
 
     # save new model
-    model.save(os.path.join(models, "standard_model"))
+    model.save(os.path.join(models, dataset + "_model"))
 
 
 
@@ -299,9 +309,9 @@ if training:
 ##########################################################################################################################
 
 
-model = keras.models.load_model(os.path.join(models, "standard_model"))
-
-#model.summary()
+if load_model:
+    model = keras.models.load_model(os.path.join(models, dataset + "_model"))
+    model.summary()
 
 
 
@@ -315,9 +325,7 @@ standard_predictions_sig = model.predict(X)
 standard_predictions_scalar = (standard_predictions_sig>0.5).astype("int32")
 
 
-metrics = False
-
-if metrics:
+if get_prediction_metrics:
     
     utils.create_metrics(y, standard_predictions_scalar)
 
@@ -327,7 +335,8 @@ if metrics:
 ##########################################################################################################################
 # add missing data
 ##########################################################################################################################
-
+#TODO
+sys.exit()
 
 # create new Dataset with random missing values
 DATAFRAME_MISS = utils.add_missing_values(DATAFRAME.iloc[:, :14], 0.1, RANDOM_STATE) 
